6e4403eae9283b7cc09409371d3daecc1c0fcc78
ImanGoudarzvand <im.goudarz@gmail.com>
2023-03-31 00:12:24 +0330

    caching with redis applied in playground app

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,17 @@
 from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 from templated_mail.mail import BaseEmailMessage
-from .task import notify_customers
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+import requests
+from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
 
-def say_hello(request):
 
-    notify_customers.delay('hello')
-
-
-    return render(request, 'hello.html', {'name': 'Mosh'})
+class HelloView(APIView):
+    
+    @method_decorator(cache_page(2*60))
+    def get(self, request):
+        response = requests.get("https://httpbin.org/delay/2") # simulate a slow api
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})
