59bdcb171fd0e8b74b2308631c5d516492301950
ImanGoudarzvand <im.goudarz@gmail.com>
2023-04-01 16:33:23 +0330

    Managing development and production settings

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -20,13 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -93,20 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'storefront.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'storefront',
-        'USER': 'postgres',
-        'PASSWORD': 'Test@123',
-        'HOST': 'localhost',
-
-    }
-}
 
 
 # Password validation
---
+++ storefront/settings/prod.py
@@ -0,0 +1,9 @@
+from .common import *
+import os
+
+DEBUG = False
+
+SECRET_KEY = os.environ["SECRETE_KEY"]
+
+ALLOWED_HOSTS = []
+
